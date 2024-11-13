4821411271441122c4982feed344a5faddbb42c3
BravSx <69966622+BravSx@users.noreply.github.com>
2024-11-13 23:10:15 +0100

    aktualizacja script.js skrócona wersja 10 linii

---
+++ 21-Social Media Dashboard/script_short.js
@@ -0,0 +1,10 @@
+
+// Przykładowe dane użytkownika
+const user = {
+    name: "John Doe",  // Imił i nazwisko userykownika
+    username: "john_doe",  // Nazmčauserkinika
+    posts: [
+        { title: "Post 1", content: "To jest treą pierwszego posta." }, // Pierwszy post uzykwodnika
+        { title: "Post 2", content: "To jest treą drugigo posta." }
+    ]
+};
