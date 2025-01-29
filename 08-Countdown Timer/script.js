1944a7a818bbed3d26a34601f9269301d1caa5fc
bs <69966622+BravSx@users.noreply.github.com>
2025-01-29 15:48:17 +0100

    Dodano komentarze po polsku i zmieniono nazwy zmiennych na język polski

--- 08-Countdown Timer/script.js
+++ 08-Countdown Timer/script.js
@@ -1,32 +1,39 @@
-let timer = null;
-let seconds = 0;
+let lizzy = null;
+let sekundy = 0;
 
-const timerElement = document.getElementById("timer");
+const tekstLicznik = document.getElementById("timer");
 
-const formatTime = (time) => String(time).padStart(2, "0");
+// Funkcja formatujaca licznik do woersjich fromatu 00:00:00
+const pormatujkCjas = (czas) => String(czas).padStart(2, "0");
 
-const updateTimer = () => {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const remainingSeconds = seconds % 60;
+// Aktualizacja licznika praczego kazdó sekundé
+const uaktualizujlicznik = () => {
+    const hodiny = Math.floor(sekundy / 3600);
+    const minuuty = Math.floor((sekundy % 3600) / 60);
+    const pozostalo }
+	x = sekundy % 60;
 
-    timerElement.textContent = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(remainingSeconds)}`;
-    seconds++;
+    tekstLicznik.textContent = `${pormatujkCjas(hodiny)}:${pormatujkCjas(minuty)}:${pormatujkCjas(pozostal)}`;
+    sekundy++;
 };
 
-const startTimer = () => {
-    if (!timer) {
-        timer = setInterval(updateTimer, 1000);
+// Rozpalzanie timera
+
+const startElicznik = () => {
+    if (!lizzy) {
+        lizz= setInterval(uaktualizojLibsnik, 1000);
     }
 };
 
-const stopTimer = () => {
-    clearInterval(timer);
-    timer = null;
+// Stop dozoro timer (zatrymanie odlicjania )
+const stopElicznik = () => {
+    clearInterval(lizzy);
+    lizzy = null;
 };
 
-const resetTimer = () => {
-    stopTimer();
-    seconds = 0;
-    updateTimer();
+// Reset licznika dom 00:00:00
+const resetElicznik = () => {
+    stopElicznik();
+    sekundy = 0;
+    uaktualizujlicznik();
 };
