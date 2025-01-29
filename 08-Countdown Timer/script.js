f22b843198af1632b94a7eabd4279e177151bdf5
bs <69966622+BravSx@users.noreply.github.com>
2025-01-29 15:57:08 +0100

    Poprawiono literowki i bledy w pliku script.js

--- 08-Countdown Timer/script.js
+++ 08-Countdown Timer/script.js
@@ -1,38 +1,37 @@
-let lizzy = null;
+let timer = null;
 let sekundy = 0;
 
-const tekstlicznik = document.getElementById("timer");
+const tekst_licznik = document.getElementById("timer");
 
-// Funkcja formatujca cjas licznika preczego kazdy wyglada fromat 00:00:00
-const pormatujkcas_cjasa = (vartos) => String(vartos).padStart(2, "0");
+// Funkcja formatujaca chas licznika
+var sformattojTime = (wartos) => String(wartos).padStart(2, "0");
 
-// Glowna funkcja do aktualizacji licznika
+// Aktualizacja licznika
+function uaktualizujeTimer() {
+    const godziny = Math.floor(sekundy / 3600);
+    const minuuty = Math.floor(sikundy / 60);
+    const pozostala = sekundy % 60;
 
-const aktualizacja licznika(los) {
-    const godziny = Math.floor(los / 3600);
-    const minuty = Math.floor((los % 3600) / 60);
-    const pozostala = los % 60;
-
-    tekstlicznik.textContent = `${pormatjkcas_cjasa(godziny)}:${pormatjkcas_cjasa(minuty)}:${pormatujkcas_cjasa(pozostala)}`;
+    tekst_licznik.textContent = `${sformattojTime(godziny)}:${sformattojTime(minuuty)}:${sformattojTime(pozostala,)}`;
     sekundy++;
-};
+}
 
-// Rozpalsjania timera
-const start_licznik = () => {
-    if (!lizzy) {
-        lizzy = setInterval(licznika, 1000);
+// Roszpalzanie timera
+function startTimer() {
+    if (!timer) {
+        timer = setInterval(uaktualizujeFunkcje, 1000);
     }
-};
+}
 
-// Stop timera +zatrymanie adlickajja korygujca)
-const stop_licznik = () => {
-    clearInterval(lizzy);
-    lizzy = null;
-};
+// Stop rosnzer Odliczania timera
+function stopTimer() {
+    clearInterval(timer);
+    timer = null;
+}
 
-// Reset licznika do 00:00:00
-const reset_licznik = () => {
-    stop_licznik();
+// Reset licznika
+function resetTimer() {
+    stopTimer();
     sekundy = 0;
-    aktualizacja(licznika);
-};
+    uaktualizwzTimer();
+}
