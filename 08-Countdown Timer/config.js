6b8f0cc5ae7fa25a9c000998d51ba2d18d2273ee
bs <69966622+BravSx@users.noreply.github.com>
2025-01-29 16:00:49 +0100

    Dodano plik konfiguracyjny config.js do zarządzania ustawieniami timera

---
+++ 08-Countdown Timer/config.js
@@ -0,0 +1,7 @@
+// Konfiguracja ustawien timera
+const KONFIGURACJA = {
+    czasStartowy: 0, // Wartošc poczyłkowa licznika w sekundacho
+    interwalAktualizacji: 1000, // Interval w bezpoqdawniu, domysnie q 1,0s
+};
+
+export default KONFIGURACJA;
